196a4e62b69824255e82f658c147aeeaedcb1fe9
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2023-10-05 23:32:53 +0300

    feat: vite, sass

---
+++ Frontend/src/components/App/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import style from './App.module.scss';
+
+function App() {
+
+  return (
+    <>
+      <main className={style.root}>
+
+      </main>
+    </>
+  )
+}
+
+export default App
