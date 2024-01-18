d602710b3d37277901d9ba0be3a0d50c0882d731
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2024-01-18 15:18:06 +0300

    add: branch feat/backend

--- Frontend/services/store/src/components/Ui/Buttons/ButtonShare/ButtonShare.tsx
+++
@@ -1,5 +0,0 @@
-function ButtonShare() {
-	return <button type="button" className="button-share"></button>;
-}
-
-export default ButtonShare;
--- Frontend/services/store/src/hooks/reduxHooks.ts
+++
@@ -1,7 +0,0 @@
-import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
-import type { RootState, AppDispatch } from '../store';
-
-// для корректной работы диспатча
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-// будет знать о структуре приложения
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
